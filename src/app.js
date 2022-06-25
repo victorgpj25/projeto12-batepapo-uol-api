bde3ef25f75bd8aa8bb215894db4a105134caf10
Victor <victorgpj25@gmail.com>
2022-06-25 06:31:23 -0300

    Post /participants

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import dotenv from "dotenv"
 import { MongoClient } from "mongodb"
 import dayjs from "dayjs"
+import joi from "joi"
 
 dotenv.config()
 
@@ -14,9 +15,47 @@ const mongoClient = new MongoClient(process.env.MONGO_URL)
 let db
 
 mongoClient.connect().then(() => {
-	db = mongoClient.db("database")
+	db = mongoClient.db("BatePapoUOL")
 })
 
+const newParticipantSchema = joi.object({
+    name: joi.string().required(),
+    lastStatus: joi.number()
+  })
+
+app.post("/participants", async (req, res) => {
+
+    const newParticipant = {
+		name: req.body.name,
+		lastStatus: Date.now()
+	}
+    const newParticipantMessage = {
+        from: req.body.name,
+        to: "Todos",
+        text: "entra na sala...",
+        type: "status", 
+        time: dayjs(Date.now()).format("HH:mm:ss")
+	}
+
+    const validation = newParticipantSchema.validate(newParticipant)
+    
+    if (validation.error) {
+        console.log(validation.error.details)
+        res.sendStatus(422)
+        return
+    }
+
+    const nameAlreadyExists = await db.collection("participants").find({name: req.body.name}).toArray()
+
+    if (nameAlreadyExists.length) {
+        res.sendStatus(409)
+        return
+    }
+
+    db.collection("participants").insertOne(newParticipant)
+    db.collection("messages").insertOne(newParticipantMessage)
+    res.sendStatus(201);
+})
 
 
 
